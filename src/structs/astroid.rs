8382e032f92638297d24dc43618c623b3f03b371
David Traum <d.traum@shopware.com>
2026-06-09 16:28:26 +0200

    wip astroid spawning

--- src/structs/astroid.rs
+++ src/structs/astroid.rs
@@ -1,6 +1,6 @@
-use macroquad::miniquad::window::screen_size;
 use macroquad::rand::gen_range;
 use macroquad::{color::Color, shapes::draw_poly_lines};
+use macroquad::window;
 
 use crate::traits::drawable::Drawable;
 use crate::traits::updatable::Updatable;
@@ -18,15 +18,21 @@ pub struct Astroid {
 
 impl Astroid {
     pub fn new_at_random_position(x: f32, y: f32) -> Self {
+        let spawn_above_screen = gen_range(0,1) > 0;
+        let spawn_left_of_screen = gen_range(0,1) > 0;
+        let max_outside_bounds = 1000.;
+        let min_outside_bounds = 100.;
+        let offset_x = if (spawn_left_of_screen) { x - window::screen_width() * 0.5 - gen_range(min_outside_bounds, max_outside_bounds ) } else { x + window::screen_height() * 0.5 + gen_range(min_outside_bounds, max_outside_bounds)};
+        let offset_y = if (spawn_above_screen) { y - window::screen_height() * 0.5 - gen_range(min_outside_bounds, max_outside_bounds) } else { y + window::screen_height() * 0.5 + gen_range(min_outside_bounds, max_outside_bounds) };
         Self {
-            x: gen_range(x - 1000., x + 1000.),
-            y: gen_range(y - 1000., y + 1000.),
+            x: offset_x,
+            y: offset_y,
             size: gen_range(10., 100.),
             sides: gen_range(5, 10),
             rotation: gen_range(0., 360.),
             rotation_speed: gen_range(-30.0, 30.0),
-            direction_x: gen_range(-1., 1.),
-            direction_y: gen_range(-1., 1.),
+            direction_x: gen_range(if offset_x < x {0.} else {1.}, if offset_x < x {1.} else {0.}),
+            direction_y: gen_range(if offset_y < y {0.} else {1.}, if offset_y < y {1.} else {0.}),
         }
     }
 }
